423ce31d5a01894f21aa7306f77b8595acbabab1
Karla Chavez <karla.tkm.96@gmail.com>
2024-05-22 16:50:20 -0300

    add /api to endpoints

--- server.js
+++ server.js
@@ -26,36 +26,36 @@ app.get('/', (req,res)=>{
 
 
 //  “/catalogo” ordenado por nombre
-//ej --> http://localhost:3008/catalogo
-app.get("/catalogo",(req,res)=>{
+//ej --> http://localhost:3008/api/catalogo
+app.get("/api/catalogo",(req,res)=>{
     res.json(getAllOrderByTitle(DB));
 })
 
 
 
-//ej --> http://localhost:3008/titulo/guasón
-//       http://localhost:3008/titulo/guas
-app.get("/titulo/:title",(req,res)=>{
+//ej --> http://localhost:3008/api/titulo/guasón
+//       http://localhost:3008/api/titulo/guas
+app.get("/api/titulo/:title",(req,res)=>{
     res.json(getTitles(req.params.title.toLowerCase()));
 })
 
 // *** ENDPOINT 3 ***
-// Ruta --> “/categoria/:cat”
+// Ruta --> “/api/categoria/:cat”
 // Crea un endpoint llamado /categoria/:cat que liste todo el contenido del archivo JSON 
 // de acuerdo a la categoría enviada como parámetro (serie o película)
 
 // *** ENDPOINT 4 ***
-// Ruta --> “/reparto/:act ”
+// Ruta --> “/api/reparto/:act ”
 // Crea un endpoint llamado /reparto/:act que liste el catálogo que incluya a la actriz o 
 //actor indicado por el nombre. (la búsqueda del nombre debe ser parcial)
 
 // *** ENDPOINT 5 ***
-// Ruta --> “/trailer/:id”
+// Ruta --> “/api/trailer/:id”
 // Crea un endpoint llamado /trailer/:id que retorne la URL del trailer de la película o serie. 
 // Si ésta no posee video asociado, que retorne un mensaje en formato JSON notificando la no disponibilidad del mismo.
 
-//ej --> http://localhost:3008/trailer/34
-app.get('/trailer/:id',(req,res)=>{
+//ej --> http://localhost:3008/api/trailer/34
+app.get('/api/trailer/:id',(req,res)=>{
     const id = parseInt(req.params.id);
     const trailer = getTrailerFind(id);
     res.send(trailer);
