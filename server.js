a441b587486e2f9cd8848f8d008a85c399021716
Melisa Encinas <mel.encinas@outlook.com>
2024-05-24 21:26:25 -0300

    endpoint 3

--- server.js
+++ server.js
@@ -1,72 +1,85 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-//MIDDLEWARE 
-const bodyParser = require('body-parser');
+//MIDDLEWARE
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-const { readTrailerflix, getTrailerFind ,getAllOrderByTitle,getTitles} = require('./src/trailerflixController');
+const {
+    readTrailerflix,
+    getTrailerFind,
+    getAllOrderByTitle,
+    getTitles,
+    getCategory,
+} = require("./src/trailerflixController");
 const PORT = process.env.PORT || 3000;
 let DB = [];
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     DB = readTrailerflix();
     next();
-})
+});
 
 // SERVIDOR WEB
 // metodo get generico
-app.get('/', (req,res)=>{
-    res.send(DB)
+app.get("/", (req, res) => {
+    res.send(DB);
 });
 
 //PARA REALIZAR!!!
 
-
 //  “/catalogo” ordenado por nombre
 //ej --> http://localhost:3008/api/catalogo
-app.get("/api/catalogo",(req,res)=>{
+app.get("/api/catalogo", (req, res) => {
     res.json(getAllOrderByTitle(DB));
-})
-
-
+});
 
 //ej --> http://localhost:3008/api/titulo/guasón
 //       http://localhost:3008/api/titulo/guas
-app.get("/api/titulo/:title",(req,res)=>{
+app.get("/api/titulo/:title", (req, res) => {
     res.json(getTitles(req.params.title.toLowerCase()));
-})
+});
 
-// *** ENDPOINT 3 ***
-// Ruta --> “/api/categoria/:cat”
-// Crea un endpoint llamado /categoria/:cat que liste todo el contenido del archivo JSON 
-// de acuerdo a la categoría enviada como parámetro (serie o película)
+// Categorías
+//Endpoint para listar el contenido según su categoría (serie o película)
+
+// ejemplo: localhost:3008/api/categoria/serie
+// ejemplo2: localhost:3008/api/categoria/pelicula
+
+app.get("/api/categoria/:cat", (req, res) => {
+    res.json(getCategory(req.params.cat.toLowerCase()));
+});
+
+//Endpoint para listar todo el contenido independientemente de su categoría
+//ruta --> "api/categoría"
+app.get("/api/categoria/", (req, res) => {
+    res.json(DB);
+});
 
 // *** ENDPOINT 4 ***
 // Ruta --> “/api/reparto/:act ”
-// Crea un endpoint llamado /reparto/:act que liste el catálogo que incluya a la actriz o 
+// Crea un endpoint llamado /reparto/:act que liste el catálogo que incluya a la actriz o
 //actor indicado por el nombre. (la búsqueda del nombre debe ser parcial)
 
 // *** ENDPOINT 5 ***
 // Ruta --> “/api/trailer/:id”
-// Crea un endpoint llamado /trailer/:id que retorne la URL del trailer de la película o serie. 
+// Crea un endpoint llamado /trailer/:id que retorne la URL del trailer de la película o serie.
 // Si ésta no posee video asociado, que retorne un mensaje en formato JSON notificando la no disponibilidad del mismo.
 
 //ej --> http://localhost:3008/api/trailer/34
-app.get('/api/trailer/:id',(req,res)=>{
+app.get("/api/trailer/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const trailer = getTrailerFind(id);
     res.send(trailer);
-})
+});
 
 // Leer las recomendaciones de: https://github.com/mariaelisaaraya/tp1ObligatorioIngenias/blob/master/readme.md
 
-
 // Mensaje de error pagina no encontrada
-app.get('*', (req, res) => {
-    res.status(404).send('Lo siento, la página que buscas no existe.'); 
+app.get("*", (req, res) => {
+    res.status(404).send("Lo siento, la página que buscas no existe.");
 });
 
 // Inicia el servidor
--- src/trailerflixController.js
+++ src/trailerflixController.js
@@ -1,65 +1,69 @@
-const { error } = require('console');
-const fs = require('fs');
+const {error} = require("console");
+const fs = require("fs");
 
-function readTrailerflix(){
-    const datos = fs.readFileSync(__dirname + process.env.TRAILERFLIX, 'utf8' )
-    const DATA = JSON.parse(datos)
-    return DATA
+function readTrailerflix() {
+    const datos = fs.readFileSync(__dirname + process.env.TRAILERFLIX, "utf8");
+    const DATA = JSON.parse(datos);
+    return DATA;
 }
 
-
-function getTrailerFind(id){
-
+function getTrailerFind(id) {
     const peliculas = readTrailerflix();
-    
-    let trailer = peliculas.find(p => p.id === id);
+
+    let trailer = peliculas.find((p) => p.id === id);
     let result;
 
     //Verifica que se haya encontrado una pelicula con el ID ingresado
     //Verifica si existe el campo trailer y si este no esta en blanco.
-    if(trailer?.id && (trailer?.trailer && trailer.trailer != '')){
+    if (trailer?.id && trailer?.trailer && trailer.trailer != "") {
         result = {
             id: trailer.id,
             titulo: trailer.titulo,
-            trailer: trailer.trailer
-        }
-    }else{
+            trailer: trailer.trailer,
+        };
+    } else {
         //Retorna un msj de error si el ID no existe o si la pelicula no tiene trailer.
-        let msj = (!trailer?.id) ? 'No se encontro la pelicula.': 'No se encontro el trailer.'; 
-        result ={
-            error: msj
-        }
+        let msj = !trailer?.id ? "No se encontro la pelicula." : "No se encontro el trailer.";
+        result = {
+            error: msj,
+        };
     }
-    
+
     return result;
 }
 
-function getAllOrderByTitle(){
-    
-    return readTrailerflix().sort((a,b)=>{
-        if ( a.titulo > b.titulo ) return 1
-        if ( a.titulo < b.titulo ) return -1
-        return 0
-    })
-
+function getAllOrderByTitle() {
+    return readTrailerflix().sort((a, b) => {
+        if (a.titulo > b.titulo) return 1;
+        if (a.titulo < b.titulo) return -1;
+        return 0;
+    });
 }
 
-
-function getTitles(titulo){
-    titulo=removeAccent(titulo)
-    let movies = readTrailerflix().filter(trailer => 
-        removeAccent(trailer.titulo.toLowerCase()).includes(titulo)
-        )
-    if(movies == "") 
+function getTitles(titulo) {
+    titulo = removeAccent(titulo);
+    let movies = readTrailerflix().filter((trailer) => removeAccent(trailer.titulo.toLowerCase()).includes(titulo));
+    if (movies == "")
         return {
-                 error:"no se encontraron coincidencias con ( " + titulo+" )" 
-                }
-    else return movies
+            error: "no se encontraron coincidencias con ( " + titulo + " )",
+        };
+    else return movies;
 }
 
-function removeAccent(cadena){
-    return cadena.replace('á','a').replace('é','e')
-                .replace('í','i').replace('ó','o').replace('ú','u')
-} 
+function removeAccent(cadena) {
+    return cadena.replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú", "u");
+}
 
-module.exports = {readTrailerflix, getTrailerFind,getAllOrderByTitle,getTitles}
+function getCategory(categoria) {
+    let movies = readTrailerflix();
+    console.log(movies);
+    let cat = removeAccent(categoria);
+    let result = movies.filter((item) => removeAccent(item.categoria.toLowerCase()).includes(cat));
+    if (result != "") {
+        return result;
+    } else {
+        return {error: "El parámetro ingresado no arrojó ningún resultado"};
+    }
+}
+
+module.exports = {readTrailerflix, getTrailerFind, getAllOrderByTitle, getTitles, getCategory};
